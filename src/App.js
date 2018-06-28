aa24859e2485d2ebe406fe25f7ef79b509c1cde0
mitchw1990 <mitchwarrenburg@yahoo.com>
2018-06-28 05:56:27 -0500

    complete menu

--- src/App.js
+++ src/App.js
@@ -6,44 +6,25 @@ class App extends Component {
     return (
       <div className="App">
 
+
         <div className="navigation">
           <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
           <label for="navi-toggle" className="navigation__button">
-            MENU
+            <span className="navigation__icon">&nbsp;</span>
           </label>
-          <div className="navigation__background">
-            &nbsp;
-          </div>
+          <div className="navigation__background">&nbsp;</div>
           <nav className="navigation__nav">
             <ul className="navigation__list">
-              <li className="navigation__item">
-                <a href="#" className="navigation__link">
-                   About Natours
-                </a>
-              </li>
-              <li className="navigation__item">
-                <a href="#" className="navigation__link">
-                   Your Benefits
-                </a>
-              </li>
-              <li className="navigation__item">
-                <a href="#" className="navigation__link">
-                  Popular Tours
-                </a>
-              </li>
-              <li className="navigation__item">
-                <a href="#" className="navigation__link">
-                  Stories
-                </a>
-              </li>
-              <li className="navigation__item">
-                <a href="#" className="navigation__link">
-                  Book Now
-                </a>
-              </li>
+              <li className="navigation__item"><a href="#" className="navigation__link">About Natours</a></li>
+              <li className="navigation__item"><a href="#" className="navigation__link">Your Benefits</a></li>
+              <li className="navigation__item"><a href="#" className="navigation__link">Popular Tours</a></li>
+              <li className="navigation__item"><a href="#" className="navigation__link">Stories</a></li>
+              <li className="navigation__item"><a href="#" className="navigation__link">Book Now</a></li>
             </ul>
           </nav>
         </div>
+
+
         <header className="header">
           <div className="header__logo-box">
             <img className="header__logo" src={require("./img/logo-white.png")} alt="Logo"/>
